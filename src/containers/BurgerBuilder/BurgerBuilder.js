4eb81bc383661bb2efa89a10d5794dadbd628c2a
Daria Dvoretskaya <ddvoretskaya@freshcode.org>
2018-09-20 17:29:37 +0300

    Refactor and optimize code and lazy loading

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -66,7 +66,12 @@ class BurgerBuilder extends Component {
   // };
 
   purchaseHandler = () => {
-    this.setState({purchasing: true})
+    if(this.props.isAuthenticated) {
+      this.setState({purchasing: true})
+    } else {
+      this.props.onSetAuthRedirectPath('/checkout');
+      this.props.history.push('/auth');
+    }
   };
 
   purchaseCancelHandler = () => {
@@ -111,6 +116,7 @@ class BurgerBuilder extends Component {
                 price={this.props.price}
                 purchaseable={this.updatePurchaseState(this.props.ings)}
                 ordered={this.purchaseHandler}
+                isAuth={this.props.isAuthenticated}
             />
           </Aux>
       );
@@ -141,7 +147,8 @@ const mapStateToProps = state => {
   return {
     ings: state.burgerBuilder.ingredients,
     price: state.burgerBuilder.totalPrice,
-    error: state.burgerBuilder.error
+    error: state.burgerBuilder.error,
+    isAuthenticated: state.auth.token !== null
   }
 };
 
@@ -150,7 +157,8 @@ const mapDispatchToProps = dispatch => {
     onIgredientAdded: (ingName) => dispatch(actions.addIngredient(ingName)),
     onIgredientRemoved: (ingName) => dispatch(actions.removeIngredient(ingName)),
     onInitIngredients: () => dispatch(actions.initIngredients()),
-    onInitPurchase: () => dispatch(actions.purchaseInit())
+    onInitPurchase: () => dispatch(actions.purchaseInit()),
+    onSetAuthRedirectPath: (path) => dispatch(actions.setAuthRedirectPath(path))
   }
 };
 
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,4 +1,5 @@
 import React,  { Component } from 'react';
+import {connect} from 'react-redux';
 
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
@@ -24,8 +25,8 @@ class Layout extends Component {
   render() {
     return (
         <Aux>
-          <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
-          <SideDrawer closed={this.sideDrawerClosedHandler}
+          <Toolbar isAuth={this.props.isAuthenticated} drawerToggleClicked={this.sideDrawerToggleHandler}/>
+          <SideDrawer isAuth={this.props.isAuthenticated} closed={this.sideDrawerClosedHandler}
           open={this.state.showSideDrawer}/>
           <main className={classes.Content}>
             {this.props.children}
@@ -35,4 +36,10 @@ class Layout extends Component {
   }
 };
 
-export default Layout;
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  }
+}
+
+export default connect(mapStateToProps)(Layout);
